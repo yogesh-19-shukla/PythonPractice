3dd406ed3438a22801ae2cc6cb336d289c3ef17c
Yogesh Shukla <yogeshshukla@Yogeshs-MacBook-Air.local>
2026-03-05 22:18:47 +0530

    added prompts

---
+++ Langchain_Prompts/messages.py
@@ -0,0 +1,18 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+messages=[
+    SystemMessage(content='You are a helpful assistant'),
+    HumanMessage(content='Tell me about LangChain')
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+
+print(messages)
